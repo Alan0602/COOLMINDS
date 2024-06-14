7701081281a81d2550823d56b0e93ce3ee1fd914
Alan K Anil <100459334+Alan0602@users.noreply.github.com>
2024-06-14 14:42:10 +0530

    Update 8.LeapYear.java
    Fix the issue

--- ASSIGNMENT1/8.LeapYear.java
+++ ASSIGNMENT1/8.LeapYear.java
@@ -1,17 +1,21 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LeapYear {
     public static void main(String[] args) {
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the year: ");
+            int year = scan.nextInt();
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the year: ");
-        int year =scan.nextInt();
-
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) { // To find it is divisible by 4 and it is not century year and it is divisible by 400
-            System.out.println(year+" is a leap year and it has 366 days");
-        } else {
-            System.out.println(year+" is not a leap year and it has 365 days");
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                System.out.println(year + " is a leap year and it has 366 days");
+            } else {
+                System.out.println(year + " is not a leap year and it has 365 days");
+            }
+            scan.close();
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.out.println(" Please enter a valid integer year.");
         }
-
     }
 }
