d678221746a557bbe602c5654c7a4e3209091095
Alan K Anil <100459334+Alan0602@users.noreply.github.com>
2024-06-25 19:39:20 +0530

    Update 1.SquareOfAsterisks.java

--- ASSIGNMENT1/1.SquareOfAsterisks.java
+++ ASSIGNMENT1/1.SquareOfAsterisks.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
-public class SquareOfAstriks {
+public class SquareOfAstriks{
     public static void main(String[] args) {
         System.out.print("Enter the value between 1 to 5:");
         Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-
-        // 1. Checking if the value is between 1 and 5
-        if (x <= 5 && x > 0) {
-            // 2. Loop for each row
-            for (int i = 0; i < x; i++) {
-                // 3. Loop for each column in a row
-                for (int j = 0; j < x; j++) {
-                    System.out.print("* ");  // Printing an asterisk followed by a space
-                }
-                System.out.println();  // 4. Moving to the next row
+        int number = scanner.nextInt();
+        scanner.close();
+        if(number<=5 && number>0){                   
+        for(int i=0; i<number; i++){      
+            for(int j=0; j<number; j++){ 
+                System.out.print("* ");
             }
-        } else {
-            System.out.println("Wrong value");
-        }
+            System.out.println();
+        }       
+    }
+    else{
+        System.out.println("Wrong Input , Try to write number from 1 to 5:");
+    }
     }
 }
